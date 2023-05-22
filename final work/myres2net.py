1a808f48a9ae347893f9f12931b9fc440e1da50f
jamie109 <2356408976@qq.com>
2023-05-22 17:38:18 +0800

    start my res2net

---
+++ final work/myres2net.py
@@ -0,0 +1,17 @@
+"""
+@File ：myres2net.py
+@Author ：jamie109
+@Date ：2023/5/22
+"""
+import torch.nn as nn
+import torch
+import math
+
+
+class Bottle2neck(nn.Module):
+    pass
+
+
+class Res2Net(nn.Module):
+    pass
+
